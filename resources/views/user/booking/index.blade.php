1c2387cac5a7e57b933e72255d9ad196d36fa508
Rahmadani Ramli <dani5087869@gmail.com>
2023-11-05 15:55:53 +0800

    pilih lapangan solved

--- resources/views/user/booking/index.blade.php
+++ resources/views/user/booking/index.blade.php
@@ -2,18 +2,19 @@
 
 @section('content')
 <div class="container content-booking">
-    <h1 class="text-center mb-5 mt-3">Pilih Lapangan</h1>
+    <h1 class="text-center title-booking">Pilih Lapangan</h1>
 
     <div class="row">
         @foreach ($fieldLists as $item)
-        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
-            <div class="card card-booking">
-                <div class="card-header">
-                    <img src="https://source.unsplash.com/1200x1200?soccer" alt="" class="img-fluid">
+        <div class="col-lg-4 col-md-6 col-sm-12 colom-content">
+            <div class="card card-lapangan">
+                <div class="img-box">
+                    <img src="https://source.unsplash.com/1200x1200?soccer" alt="">
                 </div>
-                <div class="card-body">
-                    <h5 class="text-center">{{ $item->name }}</h5>
-                    <a class="btn btn-outline-info mt-2" href="/sewa-lapangan/{{ $item->id }}/jadwal" role="button">Lihat Jadwal</a>
+                <div class="content">
+                    <h2>{{ $item->name }}</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic nesciunt corrupti harum consequatur debitis enim id.</p>
+                    <a href="/sewa-lapangan/{{ $item->id }}/jadwal">Lihat Jadwal</a>
                 </div>
             </div>
         </div>
