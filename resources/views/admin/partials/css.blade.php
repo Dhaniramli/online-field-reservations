b837581b77f617a9b7cf1270f8b956a76b1edda1
Rahmadani Ramli <dani5087869@gmail.com>
2023-11-12 22:22:30 +0800

    perbaikan warna dashboard

--- resources/views/admin/partials/css.blade.php
+++ resources/views/admin/partials/css.blade.php
@@ -1,23 +1,22 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
- 
- <!-- Custom fonts for this template-->
- <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
- <link
-     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-     rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
- <!-- Custom styles for this template-->
- <link href="/css/sb-admin-2.min.css" rel="stylesheet">
+<!-- Custom fonts for this template-->
+<link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+<link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
 
- <link href="/css/admin/sidebar.css" rel="stylesheet">
- <link href="/css/admin/topbar.css" rel="stylesheet">
- <link href="/css/admin/schedule.css" rel="stylesheet">
- <link href="/css/loading.css" rel="stylesheet">
- 
- <!-- Custom styles for this page -->
- <link href="/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+<!-- Custom styles for this template-->
+<link href="/css/sb-admin-2.min.css" rel="stylesheet">
+<link href="/css/admin/schedule.css" rel="stylesheet">
+<link href="/css/loading.css" rel="stylesheet">
+<link rel="stylesheet" href="/css/colors.css">
 
- {{-- DATATABLES --}}
+<!-- Custom styles for this page -->
+<link href="/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+{{-- DATATABLES --}}
 {{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css"> --}}
 <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,3 +1,5 @@
+<link href="/css/admin/sidebar.css" rel="stylesheet">
+
 <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
@@ -17,34 +19,6 @@
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
-{{-- 
-    <li class="nav-item {{ Request::is('admin/jam-main*') || Request::is('admin/hari-tanggal*') ? 'active' : '' }}">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne"
-            aria-expanded="true" aria-controls="collapseOne">
-            <i class="fa fa-calendar"></i>
-            <span>Waktu</span>
-        </a>
-        <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item {{ Request::is('admin/jam-main*') ? 'active' : ''}}" href="/admin/jam-main">Jam Main</a>
-                <a class="collapse-item {{ Request::is('admin/hari-tanggal*') ? 'active' : ''}}" href="/admin/hari-tanggal">Hari & Tanggal</a>
-            </div>
-        </div>
-    </li> --}}
-
-    {{-- <li class="nav-item {{ Request::is('admin/daftar-lapangan*') || Request::is('admin/jadwal-lapangan*') ? 'active' : '' }}">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-            aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fa fa-futbol"></i>
-            <span>Lapangan</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item {{ Request::is('admin/daftar-lapangan*') ? 'active' : ''}}" href="/admin/daftar-lapangan">Daftar Lapangan</a>
-                <a class="collapse-item {{ Request::is('admin/jadwal-lapangan*') ? 'active' : ''}}" href="/admin/jadwal-lapangan">Jadwal Lapangan</a>
-            </div>
-        </div>
-    </li> --}}
 
     <li class="nav-item {{ Request::is('admin/lapangan') ? 'active' : '' }}">
         <a class="nav-link " href="{{ Route('index-lapangan') }}">
@@ -53,8 +27,8 @@
     </li>
     <li class="nav-item">
         <a class="nav-link " href="/admin">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
+            <i class="fas fa-fw fa-ban"></i>
+            <span>Permintaan Pembatalan</span></a>
     </li>
 
     <!-- Divider -->
