342008663183c022d7ab423cc412d577cb8c2306
Rahmadani Ramli <dani5087869@gmail.com>
2024-05-12 00:55:40 +0800

    pwa lagi

--- config/app.php
+++ config/app.php
@@ -171,8 +171,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        \Maatwebsite\Excel\ExcelServiceProvider::class,
-        Ladumor\LaravelPwa\PWAServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -188,8 +186,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Excel' => \Maatwebsite\Excel\Facades\Excel::class,
-        'LaravelPwa' => \Ladumor\LaravelPwa\LaravelPwa::class,
     ])->toArray(),
 
     'colors' => [
